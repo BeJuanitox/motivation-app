aca5abaf21794e436190e7e7101fd7a759869cf3
Andres2D <andres2d1997.1206@gmail.com>
2023-10-24 22:36:02 -0500

    Add basic google auth

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+declare let google: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(private readonly router: Router) {}
 
   loading = signal(false);
+
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
   login(){
     this.loading.set(true);
@@ -21,4 +28,27 @@ export class LoginComponent {
     return this.loading();
   }
 
+  renderButton(): void {
+    google.accounts.id.initialize({
+      client_id: '756127147061-fqhrqhmm7shfavmjr9n95e3ade8n6qsk.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse.bind(this)
+    });
+    const options = { 
+      theme: 'filled_black',
+      text: 'continue_with',
+      shape: 'square'
+    };
+
+    google.accounts.id.renderButton(
+      document.getElementById("g_id_onload"), 
+      {...options, size: window.innerWidth <= 420 ? 'medium' : 'large'} 
+    );
+
+    google.accounts.id.prompt(); 
+  }
+
+  handleCredentialResponse(res: any): void {
+    console.log(res);
+    this.router.navigate(['/feed']);
+  }
 }
